2ec92e0e59ef2c2043e5f07c7e2386307745ce26
TuyizeeAnastase <tuyizereanastase1@gmail.com>
2024-09-09 21:44:43 +0200

    setup

---
+++ src/routers/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const routes=express()
+
+routes.get('/',(req,res)=>{
+    res.status(200).json({
+        message:"Welcome"
+    })
+})
+
+routes.get("*",(req,res)=>{
+    res.status(404).json({
+        message:"Page not found, Try again"
+    });
+});
+
+export default routes;
